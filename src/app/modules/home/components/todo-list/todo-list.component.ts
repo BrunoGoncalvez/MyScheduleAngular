74c43c843c7cbcaeae60cc1f9739d1182b117771
Bruno Gonçalves <brunosgoncalvez@gmail.com>
2022-08-04 22:30:19 -0300

    implementação da Interface TaskList e ajustes na view  todo-list

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+// Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+    {task: "My activy for today", checked: true},
+    {task: "My activy for tomorrow", checked: false}
+  ];
 
   constructor() { }
 
